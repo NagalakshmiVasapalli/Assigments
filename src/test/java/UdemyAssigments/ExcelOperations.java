14bb3efe995b17869cbd0bc5954306c4076f8305
chummy <malathi.nag7@gmail.com>
2019-10-18 21:07:57 +0530

    Modified test case

--- src/test/java/UdemyAssigments/ExcelOperations.java
+++ src/test/java/UdemyAssigments/ExcelOperations.java
@@ -19,7 +19,7 @@ public class ExcelOperations {
 		XSSFRow NewRows = NewSheet.createRow(0);
 		XSSFCell NewCells = NewRows.createCell(0);
 		NewCells.setCellValue("Online ID");
-		FileOutputStream fos = new FileOutputStream(excel);
+
 		
 		//Writing Values into New Sheet
 		
